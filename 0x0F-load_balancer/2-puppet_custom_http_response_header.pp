fadd34313d46285bb2c9084323a104f7a61597a0
yazgiraldoa <edygiraldo.97@gmail.com>
2022-01-11 16:45:54 -0500

    add script that creates a custom header woth Puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,11 @@
+# Add custom HTTP Header with Puppet
+exec { 'HTTP_header':
+  command  => 'sudo apt-get -y update;
+	       sudo apt-get -y install nginx;
+	       echo "Hello World" | sudo tee /var/www/html/index.html;
+	       sudo chmod 777 /etc/nginx;
+	       sudo chmod 777 /etc/nginx/nginx.conf;
+	       sudo sed -i "/sendfile on;/ a add_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.conf;
+               service nginx start',
+  provider => 'shell',
+}
